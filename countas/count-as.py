b643dc9b6d1114f63649c01bcddbd16d467f107c
Simon_NT <kacsa1984@gmail.com>
2017-04-10 14:43:04 +0200

    feat: function to count a letters in files. First itteration complited, not working.

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,17 @@
-def count_as(change_this):
-    pass
     # Create a function that takes a filename as string parameter,
     # counts the occurances of the letter "a", and returns it as a number.
     # If the file does not exist, the function should return 0 and not break.
+
+def count_as(file_name):
+    fr = open(file_name, "r")
+    text = fr.read()
+
+    letter_a = 0
+    for letter in file_name:
+        if letter == "a":
+            letter += 1
+    return letter_a
+
 
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
