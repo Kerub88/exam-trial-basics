d58506be2a6e0e0a5e98238f65cf954805927c04
Simon_NT <kacsa1984@gmail.com>
2017-04-10 14:31:00 +0200

    feat: a function which prints every second element of a list

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -4,4 +4,21 @@ def seconds(change_this):
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
-print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+# print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+
+
+example_list = [6, 8, 3, 4, 5]
+
+def seconds(the_list):
+    if the_list == list:
+        new_list = []
+        for element in range(0, len(the_list)):
+            if element % 2 != 0:
+                new_list.append(the_list[element])
+        return new_list
+    else:
+        return "Please add a list."
+
+
+print(seconds(example_list))
+# print(seconds(example_list))
