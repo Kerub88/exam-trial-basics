e1d21423e3ff7ad749d66cd9fd31b343ede21621
Simon_NT <kacsa1984@gmail.com>
2017-04-10 14:59:38 +0200

    fix: function working properly, I had to change the path of the text file

--- countas/count-as.py
+++ countas/count-as.py
@@ -3,15 +3,16 @@
     # If the file does not exist, the function should return 0 and not break.
 
 def count_as(file_name):
-    fr = open(file_name, "r")
-    text = fr.read()
+    try:
+        fr = open(file_name, "r")
+        text = fr.read()
+        letter_a = 1
+        for letter in text:
+            if letter == "a":
+                letter_a += 1
+        return letter_a
+    except FileNotFoundError:
+        return "0"
 
-    letter_a = 0
-    for letter in file_name:
-        if letter == "a":
-            letter += 1
-    return letter_a
-
-
-print(count_as("afile.txt")) # should print 28
+print(count_as("/Volumes/Kaleb/GreenFox/exam-trial-basics/countas/afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
